b2431e164cba2d9e6969a8143df6db4c540035dd
ATISH01 <ab.atish.ab@gmail.com>
2022-05-21 21:55:03 +0600

    login

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import * as Yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+
+import auth from '../../firebse.init';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+
+
+const Login = () => {
+    
+    const [signInWithGoogle, guser, gloading, gerror] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    const [
+        signInWithEmailAndPassword,
+        euser,
+        eloading,
+        eerror,
+      ] = useSignInWithEmailAndPassword(auth);
+      
+    const formSchema = Yup.object().shape({
+        email: Yup.string().email().required('Input Valid Email'),
+    })
+    const formOptions = { resolver: yupResolver(formSchema) }
+    const { register, handleSubmit, reset, formState } = useForm(formOptions)
+    const { errors } = formState;
+
+    const onSubmit = async userData => {
+       
+        signInWithEmailAndPassword(userData.email,userData.password);
+        console.log(userData);
+        
+        
+    }
+    const location = useLocation();
+    const from = location?.state?.from.pathname || '/';
+    
+    
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="text-center text-2xl font-bold">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-1 gap-5">
+                        <div>
+                        <input {...register("email", { required: true })} type="text" placeholder="Email" class="input input-bordered input-accent w-full max-w-xs" />
+                        <p><small>{errors.email?.message}</small></p>
+                        </div>
+                        <input  {...register("password", { required: true })} type="text" placeholder="Password" class="input input-bordered input-accent w-full max-w-xs" />
+                        <button class="btn btn-outline"  type="submit">Button</button>
+                    </form>
+                    <p>New to doctors portal? <Link to='/signup'>Create new account</Link></p>
+                    <div class="divider">OR</div>
+                    <button onClick={() => signInWithGoogle()} class="btn btn-outline">With Google</button>
+
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as Yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../firebse.init';
@@ -15,9 +15,15 @@ const Signup = () => {
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
     const [updateProfile, updating, uperror] = useUpdateProfile(auth);
-    
+
     const formSchema = Yup.object().shape({
         email: Yup.string().email().required('Input Valid Email'),
+        password: Yup.string()
+            .required('Password is mendatory')
+            .min(3, 'Password must be at 3 char long'),
+        confirmPassword: Yup.string()
+            .required('Password is mendatory')
+            .oneOf([Yup.ref('password')], 'Passwords does not match'),
     })
     const formOptions = { resolver: yupResolver(formSchema) }
     const { register, handleSubmit, reset, formState } = useForm(formOptions)
@@ -28,7 +34,7 @@ const Signup = () => {
         await createUserWithEmailAndPassword(userData.email, userData.password);
         await updateProfile({ displayName: userData.name });
     }
-    
+
     return (
         <div>
             <div className='flex justify-center items-center h-screen'>
@@ -41,13 +47,17 @@ const Signup = () => {
                                 <input {...register("email", { required: true })} type="text" placeholder="Email" class="input input-bordered input-accent w-full max-w-xs" />
                                 <p><small>{errors.email?.message}</small></p>
                             </div>
-                            <input  {...register("password", { required: true })} type="text" placeholder="Password" class="input input-bordered input-accent w-full max-w-xs" />
-                            
-                            <input  {...register("confirmPassword", { required: true })} type="text" placeholder="ConfirmPassword" class="input input-bordered input-accent w-full max-w-xs" />
-
-                            <button class="btn btn-outline" type="submit">Sign In</button>
+                            <div>
+                                <input  {...register("password", { required: true })} type="text" placeholder="Password" class="input input-bordered input-accent w-full max-w-xs"
+                                />
+                                <small className='text-red-500'>{errors.password?.message && "Password is required"}</small>
+                            </div>
+                            <input  {...register("confirmPassword", { required: true })} type="text" placeholder="ConfirmPassword" class="input input-bordered input-accent w-full max-w-xs"
+                            />
+                            <small className='text-red-500'>{errors.confirmPassword?.message}</small>
+                            <button class="btn btn-outline" type="submit">Sign Up</button>
                         </form>
-                        <p>New to doctors portal? <Link to='/login'>Already have an account?</Link></p>
+                        <p>New to doctors portal? <Link to='/login'>Already have an account? Login</Link></p>
                     </div>
                 </div>
             </div>
