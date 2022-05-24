f5e21af558426933a89eb3841abccc660e4f4757
ATISH01 <ab.atish.ab@gmail.com>
2022-05-24 11:35:41 +0600

    manage products done

--- src/Pages/DashBoard/AddProduct.js
+++ src/Pages/DashBoard/AddProduct.js
@@ -1,9 +1,76 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddProduct = () => {
+    const { register, handleSubmit, reset, formState } = useForm()
+    const { errors } = formState;
+    const onSubmit = async userData => {
+        console.log(userData);
+        const url = 'http://localhost:5000/orders';
+
+        /* fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('success', data);
+
+            })
+ */
+    }
     return (
-        <div>
-            <h1>add</h1>
+        <div className='p-6'>
+            <h1 class="text-2xl my-10 font-bold">Provide information to confirm Your Order</h1>
+            <form onSubmit={handleSubmit(onSubmit)} class="card  w-full shadow-2xl bg-base-100">
+                <div class="card-body">
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Item Name</span>
+                        </label>
+                        <input {...register("name")} type="text" placeholder="Name" class="input input-bordered" />
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Price</span>
+                        </label>
+                        <input {...register("price", { required: true })} type="text" placeholder="Name" class="input input-bordered" />
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Available Item</span>
+                        </label>
+                        <input {...register("availableItem", { required: true })} type="text" placeholder="Available Item" class="input input-bordered" />
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Quantity</span>
+                        </label>
+                        <input  {...register("quentity", { required: true })} type="text" placeholder="Phone" class="input input-bordered input-accent w-full" />
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Minimum Purse</span>
+                        </label>
+                        <input {...register("minPurse", { required: true })} type="text" placeholder="Minimum Purse" class="input input-bordered" />
+
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Image Link</span>
+                        </label>
+                        <input {...register("img", { required: true })} type="text" placeholder="Minimum Purse" class="input input-bordered" />
+
+                    </div>
+                    <div class="form-control mt-6">
+                        <button class="btn btn-primary">Purchase</button>
+                    </div>
+                </div>
+            </form>
+
         </div>
     );
 };
--- src/Pages/DashBoard/ManageProduct.js
+++ src/Pages/DashBoard/ManageProduct.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import useAllProducts from '../../Hooks/useAllProducts';
 
-const ManageProduct = () => {
+const AddProduct = () => {
+    const [products]=useAllProducts()
     return (
-        <div>
-            <h1>Manage</h1>
+        <div class="overflow-x-auto">
+            <table class="table w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                        <th>Remove Product</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {
+                        products.map(product=><tr product={product}>
+                            <th>1</th>
+                            <td>{product.name}</td>
+                            <td>{product.price}</td>
+                            <td><button class="btn btn-xs btn-error">Delete</button></td>
+                        </tr>)
+                    }
+
+                    
+                    
+                </tbody>
+            </table>
         </div>
     );
 };
 
-export default ManageProduct;
+export default AddProduct;
