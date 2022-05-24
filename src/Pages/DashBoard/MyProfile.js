e9085baaa7771c84da91dfa39250324a58ded4e7
ATISH01 <ab.atish.ab@gmail.com>
2022-05-24 22:52:02 +0600

    my profile page

--- src/Pages/DashBoard/MyProfile.js
+++ src/Pages/DashBoard/MyProfile.js
@@ -1,39 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useForm } from 'react-hook-form';
 import auth from '../../firebse.init';
 
 const MyProfile = () => {
-  const [user,email] = useAuthState(auth);
+  const [user] = useAuthState(auth);
   const { register, handleSubmit, reset, formState } = useForm();
-  const[datas,setDatas]=useState([])
+  const [datas, setDatas] = useState([]);
+  const [userData, setUserData] = useState([]);
+  const {email,customerName,education,phone,location}=datas;
+  useEffect(() => {
+    const email=(user.email);
+    console.log(email);
+    fetch(`http://localhost:5000/profile/${email}`, {
+                method:'GET'   
+            })
+      .then(res => res.json())
+      .then(data => setDatas(data))
+  }, [])
   const onSubmit = userData => {
-      console.log(userData);
-   const url = 'http://localhost:5000/profile';
-        
+    console.log(userData);
+    const url = `http://localhost:5000/profile/${user.email}`;
+
     fetch(url, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
-        body: JSON.stringify(userData)
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(userData)
     })
-        .then(res => res.json())
-        .then(result => {
-          
-            console.log(result);
-            if(result.success){
-              alert('done')
-            }
-            else{
-              alert('user exists')
-            }
-            fetch(`http://localhost:5000/profile/${user.email}`)
-            .then(res=>res.json())
-            .then(data=>setDatas(data))
-            
-        })
-        console.log(datas);
+      .then(res => res.json())
+      .then(result => {
+        if(result.acknowledged){
+          console.log('ok');
+        }
+        else{
+          console.log('no');
+        }
+
+      })
+    
 
   }
   return (
@@ -42,6 +48,9 @@ const MyProfile = () => {
         <div class="card-body">
           <h2 class="card-title">{user.displayName}</h2>
           <p>{user.email}</p>
+          <p>{education}</p>
+          <p>{location}</p>
+          <p>{phone}</p>
         </div>
       </div>
       <div>
@@ -49,15 +58,15 @@ const MyProfile = () => {
           <div class="card-body">
             <h2 class="card-title">Add or Update your information here.</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
-              <input  {...register("customerName")} value={user.displayName} type="text" placeholder="" class="hidden input input-bordered input-secondary w-full max-w-xs rounded-none" /><input  {...register("email")} type="text" value={user.email} placeholder="" class="  hidden input input-bordered input-secondary w-full max-w-xs rounded-none" />
+              <input  {...register("customerName")}  type="text" placeholder="" class="hidden input input-bordered input-secondary w-full max-w-xs rounded-none" /><input  {...register("email")} type="text"  placeholder="" class="  hidden input input-bordered input-secondary w-full max-w-xs rounded-none" />
               <label htmlFor="">Education</label>
-              <input  {...register("education")} type="text" placeholder="" class="input input-bordered input-secondary w-full max-w-xs rounded-none" />
+              <input   {...register("education")} required type="text" placeholder="" class="input input-bordered input-secondary w-full max-w-xs rounded-none" />
               <label htmlFor="">Location/City</label>
-              <input {...register("location")} type="text" placeholder="Type here" class="input input-bordered input-secondary w-full max-w-xs rounded-none" />
+              <input  {...register("location")} required type="text" placeholder="Type here" class="input input-bordered input-secondary w-full max-w-xs rounded-none" />
               <label htmlFor="">Phone</label>
-              <input {...register("phone")} type="text" placeholder="Type here" class="input input-bordered input-secondary w-full max-w-xs rounded-none" />
+              <input  {...register("phone")} required type="text" placeholder="Type here" class="input input-bordered input-secondary w-full max-w-xs rounded-none" />
               <label htmlFor="">LinkDin</label>
-              <input {...register("linkdin")} type="text" placeholder="Type here" class="input input-bordered input-secondary w-full max-w-xs rounded-none" />
+              <input {...register("linkdin")} required type="text" placeholder="Type here" class="input input-bordered input-secondary w-full max-w-xs rounded-none" />
               <button className='btn btn-secondary mt-3' type='submit'>Submit</button>
             </form>
           </div>
